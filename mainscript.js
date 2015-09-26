3d353e9b247e439f48a165c2ad2f5d83393d9ec0
PixelBreeze <thepixelbreeze@gmail.com>
2015-09-27 01:10:21 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,2 +1,13 @@
-# NC-Potato
-$('body').append('<style> .uname.rank-2 {color:' + colorb + ' !important} </style>')
+var colorR = '#00CEFF'; //color RDj
+var colorB = '#0058FF'; //color Bouncer
+var colorM = '#FF00F5'; //color Manager
+var colorCH = '#FF005C'; //color Cohost
+var colorH = '#FF002D'; //color Host
+  $('body').append('<style> .uname.rank-1 {color:' + colorR + ' !important} </style>')
+  $('body').append('<style> .uname.rank-2 {color:' + colorB + ' !important} </style>')
+  $('body').append('<style> .uname.rank-3 {color:' + colorM + ' !important} </style>')
+  $('body').append('<style> .uname.rank-4 {color:' + colorCH + ' !important} </style>')
+  $('body').append('<style> .uname.rank-5 {color:' + colorH + ' !important} </style>')
+
+
+
